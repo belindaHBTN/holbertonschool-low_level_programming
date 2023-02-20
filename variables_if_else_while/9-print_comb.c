d68ea27ce1900dda8f954a9a7395de91ab21ae81
belindaHBTN <6372@holbertonstudents.com>
2023-02-19 20:13:52 -0800

    printed 0-9 with comma and space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar('\n');
+		}
+		i++;
+	}
+	return (0);
+}
