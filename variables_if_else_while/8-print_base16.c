f7c05daaea465285f0c58f3321a862549963b817
belindaHBTN <6372@holbertonstudents.com>
2023-02-19 22:01:24 -0800

    printed all the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	j = 97;
+	while (j <= 102)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
