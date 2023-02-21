5c4330e2228d5cb3db98c3a867bab69a3ad6f6d7
belindaHBTN <6372@holbertonstudents.com>
2023-02-20 20:21:44 -0800

    deleted the prototype from the program, then added it to main.h

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entryp point: print out a-z
+ * print_alphabet - prints out a-z
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-
 void print_alphabet(void)
 {
 	int i = 97;
-	while ( i <= 122)
+
+	while (i <= 122)
 	{
 		_putchar(i);
 		i = i + 1;
