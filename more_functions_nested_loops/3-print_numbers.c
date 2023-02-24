1cc5ea7473075ef529bb3a3d9c4ca26c62c66eb4
belindaHBTN <6372@holbertonstudents.com>
2023-02-23 19:00:56 -0800

    prints number from 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0-9.
+ *
+ * Description: print numbers from 0-9.
+ * Return: 0 successful.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i = i + 1;
+	}
+	_putchar('\n');
+}
