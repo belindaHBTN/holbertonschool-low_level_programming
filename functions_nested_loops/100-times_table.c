94f427b18fbc60f640fe5b28cf9e80b0fcc35d67
belindaHBTN <6372@holbertonstudents.com>
2023-02-24 22:34:51 -0800

    fixed negative digits

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * times_table - prints the n times table.
+ * print_times_table - prints the n times table.
  * @n: number
  *
  * Description: prints the n times table.
@@ -11,17 +11,14 @@ void print_times_table(int n)
 {
 	int line_num;
 	int num;
-	int total_num;
+	int tn;
 
-	if (n > 15 || n < 0)
-	{
-		_putchar('\n');
-	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar('0');
+		_putchar('\n');
 	}
-	else
+	else if (n > 0 && n <= 15)
 	{
 		line_num = 0;
 		while (line_num <= n)
@@ -29,34 +26,34 @@ void print_times_table(int n)
 			num = 0;
 			while (num <= n)
 			{
-				total_num = line_num * num;
+				tn = line_num * num;
 				if (num == 0)
 				{
 					_putchar('0');
 				}
-				else if (total_num >= 0 && total_num <= 9)
+				else if (tn >= 0 && tn <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(total_num + '0');
+					_putchar(tn + '0');
 				}
-				else if (total_num >= 10 && total_num <= 99)
+				else if (tn >= 10 && tn <= 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((total_num / 10) + '0');
-					_putchar((total_num % 10) + '0');
+					_putchar((tn / 10) + '0');
+					_putchar((tn % 10) + '0');
 				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((total_num / 100) + '0');
-					_putchar((total_num / 10 % 10) +'0');
-					_putchar((total_num % 100 % 10) + '0');
+					_putchar((tn / 100) + '0');
+					_putchar((tn / 10 % 10) + '0');
+					_putchar((tn % 100 % 10) + '0');
 				}
 				num = num + 1;
 			}
