c40e54f9a1893565b24e8f91c4bc604401f7f042
belindaHBTN <6372@holbertonstudents.com>
2023-03-03 14:30:53 -0800

    updated the formate

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,35 +1,40 @@
 #include "main.h"
 
 /**
- * _strspn: get the length of a prefix substring.
+ * _strspn - get the length of a prefix substring.
  * @s: string
  * @accept: prefix substring.
  *
  * Description: get the length of a prefix substring.
+ * Return: unsigned integer
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i = 0;
-    unsigned int count = 0;
-    
-    while (*(s + i) != '\0')
-    {
-        if ((*(s + i) >= 'a' && *(s + i) <= 'z') || (*(s + i) >= 'A' && *(s + i) <= 'Z'))
-        {
-            int j = 0;
-            while (*(accept + j) != '\0')
-            {
-                if (*(s + i) == *(accept + j))
-                {
-                    count = count + 1;
-                }
-                j = j + 1;
-            }
-            i = i + 1;
-        }
-        else {
-            break;
-        }
-    }
-    return (count);
+	int i;
+	int j;
+	unsigned int count = 0;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 'a' && *(s + i) <= 'z')
+		|| (*(s + i) >= 'A' && *(s + i) <= 'Z'))
+		{
+			j = 0;
+			while (*(accept + j) != '\0')
+			{
+				if (*(s + i) == *(accept + j))
+				{
+					count = count + 1;
+				}
+				j = j + 1;
+			}
+			i = i + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (count);
 }
