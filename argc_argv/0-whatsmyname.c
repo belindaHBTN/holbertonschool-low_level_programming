cd7fa0ace50232828967635587762d14b7a4cd3f
belindaHBTN <6372@holbertonstudents.com>
2023-03-07 01:55:45 -0800

    print the program's name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: a count of the arguments passed to the program
+ * @argv: an array of pointers to the strings which are those args
+ *
+ * Description: print the name of the program
+ * Return: Always be 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
