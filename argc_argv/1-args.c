16bf0719987606023ca50ac9a12819e0a0629828
belindaHBTN <6372@holbertonstudents.com>
2023-03-08 22:36:51 -0800

    updated the way to void the unused parameter

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,9 +9,8 @@
  * Return: Always be 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	(void)argv;
 	printf("%d\n", (argc - 1));
 	return (0);
 }
