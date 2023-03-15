9a79af927f67321adb46ffbf1b1ed73588b10041
belindaHBTN <6372@holbertonstudents.com>
2023-03-15 00:02:05 -0700

    a function tha tconcatenates two strings

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - calculate the length of the string.
+ * @str: string (array of chars)
+ *
+ * Description: calculate the length of the string.
+ * Return: the length (integer)
+ */
+
+unsigned int _strlen(char *str)
+{
+	unsigned int len;
+
+	len = 0;
+	while (*(str + len) != '\0')
+	{
+		len = len + 1;
+	}
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenate two strings.
+ * @s1: the first string.
+ * @s2: the second string.
+ * @n: the number of characters should be copied from s2.
+ *
+ * Description: concatenate two strings.
+ * Return: pointer that point to a newly allocated space in memory.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, i, j;
+	char *p;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	if (n >= len2)
+		n = len2;
+
+	p = malloc(sizeof(char) * (len1 + n + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len1)
+	{
+		*(p + i) = *(s1 + i);
+		i = i + 1;
+	}
+	j = 0;
+	while (j < n)
+	{
+		*(p + i + j) = *(s2 + j);
+		j = j + 1;
+	}
+	return (p);
+}
