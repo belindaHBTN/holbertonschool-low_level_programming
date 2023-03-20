e966c8318249d0b66dce503bc1e538f7f9d87668
belindaHBTN <6372@holbertonstudents.com>
2023-03-20 03:36:35 -0700

    initilized dog structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a dog structure
+ * @d: struc dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ *
+ * Description: initialize a dog structure
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
