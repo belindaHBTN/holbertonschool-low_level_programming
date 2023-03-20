4dc11f2a083c65fb4603cd72c6a79c28083b47da
belindaHBTN <6372@holbertonstudents.com>
2023-03-20 03:46:08 -0700

    checked the NULL pointer

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize a dog structure
@@ -13,7 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
