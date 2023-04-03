64dfd0bb9f989df12252c862b5ed83485a508385
belindaHBTN <6372@holbertonstudents.com>
2023-04-02 22:55:19 -0700

    updated the type of return

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int i;
+	size_t i;
 
 	if (h == NULL)
 	{
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
-		{
+		}
 		h = h->next;
 		i = i + 1;
 	}
