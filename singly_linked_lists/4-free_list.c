3e5f46b4c3511470938f46860d9e53f7d8907564
belindaHBTN <6372@holbertonstudents.com>
2023-04-03 05:57:01 -0700

    free a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @h: head node
+ *
+ * Description: frees a list_t list
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *pCurrent;
+	list_t *pNext;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	pCurrent = head;
+	while (pCurrent != NULL)
+	{
+		pNext = pCurrent->next;
+		free(pCurrent);
+		pCurrent = pNext;
+	}
+}
