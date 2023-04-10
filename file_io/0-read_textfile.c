6eb6f9bb291293356a5d18b8f2c37524734141dd
belindaHBTN <6372@holbertonstudents.com>
2023-04-10 06:28:21 -0700

    fixed betty, added free malloc

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: the path of the file
+ * @letters: the number of letters if should read and print
  *
- *
+ * Description: reads a text file and prints it to the standard output
+ * Return: the actual number of letters it could read and print.
+ * or 0 if it fails
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -28,6 +33,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	nobw = write(STDOUT_FILENO, buf, nobr);
 
 	close(fd);
-	
+	free(buf);
 	return (nobw);
 }
