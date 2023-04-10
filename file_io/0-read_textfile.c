d89b462e4be28a78013ab864b6da619678cb4b08
belindaHBTN <6372@holbertonstudents.com>
2023-04-10 06:37:48 -0700

    check if the file can be opened sucessfully

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -22,6 +22,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
 
 	buf = malloc((sizeof(*buf)) * letters);
 	if (buf == NULL)
