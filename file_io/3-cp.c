fff7616373ec70c825be6f6af240493ca7c9110a
belindaHBTN <6372@holbertonstudents.com>
2023-04-11 00:31:49 -0700

    a program that copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,103 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text at the end of a file
+ * @filename: the name of the file need to be created
+ * @text_content: the content that will be wrote to the file
+ *
+ * Description: a function that append text at the end of a file
+ * Return: 1 on success, -1 on failure
+ */
+int ropen(const char *pathname, int flags)
+{
+	int fd;
+
+	fd = open(pathname, flags);
+	if (pathname == NULL || fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", pathname);
+		exit(98);
+	}
+	return (fd);
+}
+
+int wopen(const char *pathname, int flags, mode_t mode);
+{
+	int fd;
+
+	fd = open(pathname, flags, mode);
+	if (pathname == NULL || fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", pathname);
+		exit(99);
+	}
+	return (fd);
+}
+
+int close(int fd, fdvalue)
+{
+	int res;
+
+	res = close(fd)
+	if (res = -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", fdvalue);
+		exit(100);
+	}
+	return (res);
+}
+
+ssize_t xread(int fd, char *buf, size_t count, char *name_of_file)
+{
+	ssize_t bytes_read;
+
+	bytes_read = read(fd, buf, count);
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name_of_file);
+		exit(98);
+	}
+	return (bytes_read);
+}
+
+ssize_t xwrite(int fd, char *buf, size_t count, char *name_of_file)
+{
+	ssize_t bytes_write;
+
+	bytes_write = write(fd, buf, count);
+	if (bytes_write == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name_of_file);
+		exit(99);
+	}
+	return (butes_write);
+}
+
+int main(int argc, char *argv[])
+{
+	int fd_from;
+	int fd_to;
+	char buf[1024];
+	int bytes_read;
+	int bytes_write;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = ropen(argv[1], O_RDONLY);
+	fd_to = wopen(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	bytes_read = xread(fd_from, buf, 1024, argv[1]);
+	while (bytes_read != 0)
+	{
+		bytes_write = xwrite(fd_to, buf, bytes_read, argv[2]);
+		bytes_read = xread(fd_from, buf, 1024, argv[1]);
+	}
+
+	close(fd_from, argv[1]);
+	close(fd_to, argv[2]);
+	return (0);
+}
