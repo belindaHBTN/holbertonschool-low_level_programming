7bbbea42bd6c0829f97ff4cd33d326fc51a5645f
belindaHBTN <6372@holbertonstudents.com>
2023-04-10 18:07:48 -0700

    a function to create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file need to be created
+ * @text_content: the content that will be wrote to the file
+ *
+ * Description: a function that creates a file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len;
+	int nobw;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+		{
+			len = len + 1;
+		}
+		nobw = write(fd, text_content, len);
+	}
+	if (nobw == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
