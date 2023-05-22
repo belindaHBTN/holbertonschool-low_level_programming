dc23a5b2164d3ec754a544705938d4dd298669c1
belindaHBTN <6372@holbertonstudents.com>
2023-05-21 21:50:13 -0700

    fixed betty problem

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for a value in an array of integers.
+ * @array: a pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: the value to search for.
+ *
+ * Return: Always EXIT_SUCCESS
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
