4e45bde201f712d749f2a9f1d8a509161e116e39
belindaHBTN <6372@holbertonstudents.com>
2023-05-24 00:09:01 -0700

    updated header file && added pint function

--- monty/main.c
+++ monty/main.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv)
 	instruction_t opcodes[] = {
 		{"push", push},
 		{"pall", pall},
+		{"pint", pint},
 		{NULL, NULL}
 	};
 
--- monty/monty.h
+++ monty/monty.h
@@ -45,6 +45,7 @@ void free_stack(stack_t *stack);
 int is_integer(char *str);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 
 #endif /* MONTY_H */
